bd8b3f5190b010469a7ead9c3698e00b42adb470
Kevin <kevin/nave@viacesi.fr>
2020-10-29 16:23:29 +0100

    ajout menu vertical

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,30 @@
   <div id="app">
     <v-app id="inspire">
       <v-navigation-drawer v-model="drawer" app>
-        <!--  -->
+        <v-sheet color="grey lighten-4" class="pa-4">
+          <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+          <div>Username</div>
+        </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="[icon, text] in links" :key="icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
       </v-navigation-drawer>
 
       <v-app-bar app>
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-        <v-toolbar-title>Application</v-toolbar-title>
+        <v-toolbar-title>MADERA</v-toolbar-title>
       </v-app-bar>
 
       <v-main>
@@ -26,7 +43,7 @@
   text-align: center;
   color: #2c3e50;
 }
-
+/*
 #nav {
   padding: 30px;
 }
@@ -38,11 +55,20 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+}*/
 </style>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({ 
+      drawer: null,
+      links: [
+        ['mdi-delete', 'Projets'],
+        ['mdi-inbox-arrow-down', 'Devis'],
+        ['mdi-send', 'Clients'],
+        ['mdi-delete', 'Catalogue'],
+        ['mdi-alert-octagon', 'A propos'],
+      ],}),
+    
   }
 </script>
