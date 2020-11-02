e09e237320406cf47dda8820f9e98cf0b8ba3730
Kevin <kevin/nave@viacesi.fr>
2020-11-02 15:22:57 +0100

    ajout Pouchdb + Shortid

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import PouchDB from 'pouchdb'
 
 Vue.config.productionTip = false
 
+// PouchDB
+const db = new PouchDB('madera');
+Vue.prototype.$db = db;
+const token = btoa('');
+Vue.prototype.$token = token;
+const api = ''
+Vue.prototype.$api = api;
+
+//SHORTID
+const shortid = require('shortid');
+shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
+Vue.prototype.$shortid = shortid
+
+// Router + Vuetify
 new Vue({
   router,
   vuetify,
